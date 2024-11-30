c9a12876f982ca0d53caeb61277d4f367b81854d
Illangovan <andrewillango1212@gmail.com>
2024-11-29 21:21:00 -0500

    update

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -4,6 +4,14 @@ function Certificate() {
   return (
     <>
     <h1 className='certificate_heading'><span>#</span>certifications</h1>
+    <div className="certificate_container">
+      <div className="certificate_card">
+            <h3>Working on this section</h3>
+      </div>
+      <div className="certificate_card">
+      <h3>Working on this section</h3>
+      </div>
+    </div>
     <hr></hr>
     </>
   )
